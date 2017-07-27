158c4eddde3c7286f7d34cda5f21a7bcaf74c072
Michel Goñi <michelgoni2@gmail.com>
2017-07-27 19:40:09 +0200

    Added new xib for the languages module

--- ShowmeSomeNetflix/LanguagesModuleViewController.swift
+++ ShowmeSomeNetflix/LanguagesModuleViewController.swift
@@ -9,11 +9,44 @@
 import UIKit
 
 class LanguagesModuleViewController: UIViewController {
+    
+    @IBOutlet weak var popUpView: UIView!
+    
     var presenter: LanguagesModulePresenter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    //MARK:-View and animations items
+    open func showInView(_ aView: UIView!, withImage image : UIImage?, withMessage message: String?, animated: Bool) {
+        
+        aView.addSubview(self.view)
+        
+        if animated {
+            self.showAnimate()
+        }
+    }
+    
+    func showAnimate(){
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    @IBAction func closePopUpView(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished){
+                self.view.removeFromSuperview()
+            }
+        });
     }
 }
 
